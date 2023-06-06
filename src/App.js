9c6e6be9523554918480b6b98196da1519291dac
austin-carman <abcarman12@gmail.com>
2023-06-05 14:47:08 -1000

    basic structure for home file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Game Center</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <>
+      <h2>Game Center</h2>
+      <div>
+        <div>
+          <img src="#" />
+          <h4>Connect Four</h4>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
